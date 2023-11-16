87200b4de2557cfa00f9dc3ac4ce473a3a0609d0
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-16 21:37:58 +0100

    Update update.sql

--- update.sql
+++ update.sql
@@ -7,17 +7,17 @@ SET nachname = 'Dampf-Wolke'
 WHERE vorname = 'Lilli' AND nachname = 'Wolke' AND email = 'lilli@wolke.de';
 
 ALTER TABLE Benutzer
-ADD COLUMN alter INT;
+ADD COLUMN age INT;
 
 UPDATE Benutzer
-SET alter = 31
+SET age = 31
 WHERE vorname = 'Hans' AND nachname = 'Dampf-Wolke' AND email = 'hans@dampf.de';
 
 UPDATE Benutzer
-SET alter = 28
+SET age = 28
 WHERE vorname = 'Lilli' AND nachname = 'Dampf-Wolke' AND email = 'lilli@wolke.de';
 
-INSERT INTO Benutzer (vorname, nachname, email, alter)
+INSERT INTO Benutzer (vorname, nachname, email, age)
 VALUES
   ('Max', 'Mustermann', 'max@mustermann.de', 25),
   ('Anna', 'Schmidt', 'anna@schmidt.de', 30),
